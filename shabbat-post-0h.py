8e97124f1337aa616b4eb0003e1369b29c7a3e86
Dov Grobgeld <dov.grobgeld@gmail.com>
2026-07-17 09:06:57 +0300

    Update the pre-.5h and post-0h scripts

--- shabbat-post-0h.py
+++ shabbat-post-0h.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from shabbat_prep_tools import play_mp3, open_log
+from shabbat_prep_tools import play_mp3, open_log, mqtt_query
 import sys
 
 logger = open_log(sys.argv[0])
@@ -9,4 +9,7 @@ logger = open_log(sys.argv[0])
 url = 'http://192.168.1.11/hamavdil-marokai.mp3'
 play_mp3(url=url,
          volume=7)
+
+# Turn off the kumkum
+mqtt_query('kumkum', action='OFF')
 print('ok')
--- shabbat-pre-0.5h.py
+++ shabbat-pre-0.5h.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python3
 
-from shabbat_prep_tools import play_mp3, open_log
+from shabbat_prep_tools import play_mp3, open_log, check_is_up, mqtt_query, send_msg
 import sys
 
+kumkum_topic = 'kumkum'
+#kumkum_topic = 'tasmota_sonoff_hd_power'
+audio_fail = 'kumkum-lo-mechubar-0.5h.mp3'
+audio_succeed = 'kolhakavod-kumkum.mp3'
+
 logger = open_log(sys.argv[0])
+reason = None
+try:
+  is_up= mqtt_query(kumkum_topic)=='ON'
+  print(f'{is_up=}')
+  if not is_up:
+    reason = 'Switch connected but not turned on'
 
-# TBD
+except TimeoutError:
+  is_up = False
+  reason = 'Timeout error'
 
-#url = 'http://192.168.1.11/shavua-tov.mp3'
-#play_mp3(url=url,
-#         volume=5)
+if reason:
+  send_msg(f'shabbat-pre-0.5h: {reason}')
+  play_mp3(f'http://192.168.1.11/{audio_fail}')
+else:
+  play_mp3(f'http://192.168.1.11/{audio_succeed}')
+  send_msg(f'kumkum ok!')
+print(f'{is_up=}')
 
 print('ok')
-
